52ef8667e42458eb2400a31322de9416a40638da
DanNisenson <danpablo1@gmail.com>
2023-02-28 13:27:06 +0100

    add Authorization headers to PaymentGateway reqs

--- src/Services/PaymentGateway.js
+++ src/Services/PaymentGateway.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 
 class Gateway {
+  static getConfig = (token) => {
+    return { headers: { Authorization: token } }
+  }
+
   static getProduct = async (productUuid) => {
+    const token = localStorage.getItem('token')
+    const config = this.getConfig(token)
     // try {
-    const res = await axios.get(`${process.env.REACT_APP_BASE_URL}/private/product/${productUuid}`)
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}/private/product/${productUuid}`,
+      config,
+    )
     if (res.status === 200) {
       return res
     }
@@ -13,10 +22,14 @@ class Gateway {
   }
 
   static confirmPayment = async (transactionInfo) => {
+    const token = localStorage.getItem('token')
+    const config = this.getConfig(token)
+
     try {
       const res = await axios.post(
         `${process.env.REACT_APP_BASE_URL}/private/transaction`,
         transactionInfo,
+        config,
       )
       if (res.status === 201) {
         return res
